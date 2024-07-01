3760ba8487b38c820d07efbe5cf867b7719bfe71
MAanaskhan <onlineforyou33@gmail.com>
2024-07-01 12:29:45 +0500

    added script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+console.log("script running.....")
+document.querySelector('.cross').style.display="none";
+document.querySelector('.ham').addEventListener("click", ()=>{
+    document.querySelector('.sidebar').classList.toggle('sidebarGo');
+    if( document.querySelector('.sidebar').classList.contains('.sidebarGo')){
+        document.querySelector('.ham').style.display="inline"
+        document.querySelector('.cross').style.display="none"
+    }
+    else {
+        document.querySelector('.ham').style.display="none"
+        setTimeout(()=>{
+        document.querySelector('.cross').style.display="inline"
+        },300);
+    }
+});
+document.querySelector('.cross').addEventListener("click", ()=>{
+    document.querySelector('.sidebar').classList.toggle('sidebarGo');
+    if( document.querySelector('.sidebar').classList.contains('sidebarGo')){
+        document.querySelector('.ham').style.display="inline"
+        document.querySelector('.cross').style.display="none"
+    }
+    else {
+        document.querySelector('.ham').style.display="none"
+        document.querySelector('.cross').style.display="inline"
+    }
+})
